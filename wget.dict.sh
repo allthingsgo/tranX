05553e72ff23e0ff387fa297693fe5c12cc3d550
sye <badvogato@gmail.com>
2014-06-07 02:08:40 -0400

    add HuZheng StarDict

---
+++ wget.dict.sh
@@ -0,0 +1,61 @@
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-mdbg-cc-cedict-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-naver_krcn-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-naver_cnkr-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-viet_han-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-han_viet-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-FanGaoZuoPin-3.0.0.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-wubi86-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-zigenzidian-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-bcgm-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-XiaoXueTang-jc-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-XiaoXueTang-cj-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-jc-special-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-kdic-jc-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-kdic-cj-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-fccf-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-woaifayu-fi-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-xindehan-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-yusig-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-soothill-buddhist-gb-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-kdic-computer-gb-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-21shijishuangyukejicidian-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-quick_eng-zh_CN-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-xiangya-medical-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-hanzim-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-ProECCE-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-langdao-ce-gb-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-langdao-ec-gb-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-cedict-gb-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-xdict-ce-gb-2.4.2.tar.bz2
+
+wget http://abloz.com/huzheng/stardict-dic/zh_CN/stardict-xdict-ec-gb-2.4.2.tar.bz2
+
+
